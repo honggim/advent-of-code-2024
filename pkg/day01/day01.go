9f40e8b0f388372da0cb533419e34bee3214bf97
hong <hong.gim@iheartjane.com>
2024-12-03 19:49:24 -0800

    refactor: math package

--- pkg/day01/day01.go
+++ pkg/day01/day01.go
@@ -1,49 +1,42 @@
 package day01
 
 import (
+	"advent-of-code-2024/pkg/math"
 	"errors"
 	"sort"
 )
 
-func part1(left, right []int) (int, error) {
-	if len(left) != len(right) {
+func part1(lefts, rights []int) (int, error) {
+	if len(lefts) != len(rights) {
 		return -1, errors.New("unequal number of elements")
 	}
 
 	sum := 0
 
-	sort.Ints(left)
-	sort.Ints(right)
+	sort.Ints(lefts)
+	sort.Ints(rights)
 
-	for i, l := range left {
-		r := right[i]
-		sum += diff(l, r)
+	for i, left := range lefts {
+		right := rights[i]
+		sum += math.AbsDiff(left, right)
 	}
 
 	return sum, nil
 }
 
-func diff(x, y int) int {
-	diff := x - y
-	if diff < 0 {
-		return -diff
-	}
-	return diff
-}
-
-func part2(left, right []int) int {
+func part2(lefts, rights []int) int {
 	hash := make(map[int]int)
 
-	for _, r := range right {
-		hash[r]++
+	for _, right := range rights {
+		hash[right]++
 	}
 
 	sum := 0
 
-	for _, l := range left {
-		multiplier, ok := hash[l]
+	for _, left := range lefts {
+		multiplier, ok := hash[left]
 		if ok {
-			sum += (l * multiplier)
+			sum += (left * multiplier)
 		}
 	}
 
--- pkg/day02/day02.go
+++ pkg/day02/day02.go
@@ -1,5 +1,9 @@
 package day02
 
+import (
+	"advent-of-code-2024/pkg/math"
+)
+
 func part1(levels [][]int) int {
 	sum := 0
 	for _, level := range levels {
@@ -14,32 +18,19 @@ func evaluate(level []int) bool {
 	var xor bool
 	for i := 0; i < len(level)-1; i++ {
 		if i == 0 {
-			xor = isPositive(level[i], level[i+1])
+			xor = !math.IsNegative(level[i], level[i+1])
 		}
-		if xor != isPositive(level[i], level[i+1]) {
+		if xor != !math.IsNegative(level[i], level[i+1]) {
 			return false
 		}
 
-		abs := absDiff(level[i], level[i+1])
+		abs := math.AbsDiff(level[i], level[i+1])
 		if abs == 0 || abs > 3 {
 			return false
 		}
 	}
 
 	return true
-}
-
-// TODO: create separate package
-func absDiff(x, y int) int {
-	diff := x - y
-	if diff < 0 {
-		return -diff
-	}
-	return diff
-}
-
-func isPositive(x, y int) bool {
-	return (x - y) >= 0
 }
 
 func part2(levels [][]int) int {
