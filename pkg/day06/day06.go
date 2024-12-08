62475c9eab55323db4b3f605f59f4a0e4568b9c9
hong <hong.gim@iheartjane.com>
2024-12-07 17:32:04 -0800

    part 2

--- pkg/day06/day06.go
+++ pkg/day06/day06.go
@@ -19,56 +19,113 @@ const (
 	W
 )
 
+type Position struct {
+	Point
+	Direction
+}
+
 type Game struct {
 	puzzle     [][]rune
 	yMax, xMax int
-	position   Point
-	facing     Direction
+
+	position Position
 
 	traveled map[string]bool
+	rocks    map[string]bool
 }
 
 func New(puzzle [][]rune, x, y int) *Game {
-	start := Point{x, y}
+	position := Position{
+		Point: Point{
+			x: x,
+			y: y,
+		},
+		Direction: N,
+	}
 	traveled := map[string]bool{
-		start.Token(): true,
+		position.Token(): true,
 	}
 
 	return &Game{
 		puzzle:   puzzle,
 		yMax:     len(puzzle) - 1,
 		xMax:     len(puzzle[0]) - 1,
-		position: start,
-		facing:   N,
+		position: position,
 		traveled: traveled,
+		rocks:    make(map[string]bool),
 	}
 }
 
-func (g *Game) Count() int {
+func (g *Game) TravelDistance() int {
 	return len(g.traveled)
 }
 
-func (g *Game) Simulate() int {
+func (g *Game) Simulate() (int, bool) {
 	for !g.isEnd() {
-		switch g.peek() {
+		next := g.next()
+		switch g.peek(next) {
 		case '.':
 			g.move()
 		case '^':
 			g.move()
 		case '#':
+			rockAddy := fmt.Sprintf("%s|%v", next.Token(), g.position.Direction)
+			if ok := g.rocks[rockAddy]; ok {
+				return 0, false
+			}
+			g.rocks[rockAddy] = true
 			g.turn()
 		}
 	}
 
-	return g.Count()
+	return g.TravelDistance(), true
 }
+
+/*
+// what if next '.' was changed to '#'
+// check right turn direction for previous rock
+func (g *Game) potentialLoop() bool {
+	switch g.position.Direction {
+	case N: // check E, x+
+		for x := g.position.Point.x; x <= g.xMax; x++ {
+			token := Point{x: x, y: g.position.y}.Token()
+			if g.rocks[token] {
+				return true
+			}
+		}
+	case E: // check S, y+
+		for y := g.position.Point.y; y <= g.yMax; y++ {
+			token := Point{x: g.position.x, y: y}.Token()
+			if g.rocks[token] {
+				return true
+			}
+		}
+	case S: // check W, x-
+		for x := g.position.Point.x; x >= 0; x-- {
+			token := Point{x: x, y: g.position.y}.Token()
+			if g.rocks[token] {
+				return true
+			}
+		}
+	case W: // check N, y-
+		for y := g.position.Point.y; y >= 0; y-- {
+			token := Point{x: g.position.x, y: y}.Token()
+			if g.rocks[token] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+*/
 
 func (g *Game) next() (p Point) {
 	next := Point{
 		x: g.position.x,
 		y: g.position.y,
 	}
-	switch g.facing {
+	switch g.position.Direction {
 	case N:
 		next.y--
 	case E:
@@ -83,37 +140,35 @@ func (g *Game) next() (p Point) {
 }
 
 func (g *Game) turn() {
-	switch g.facing {
+	switch g.position.Direction {
 	case N:
-		g.facing = E
+		g.position.Direction = E
 	case E:
-		g.facing = S
+		g.position.Direction = S
 	case S:
-		g.facing = W
+		g.position.Direction = W
 	case W:
-		g.facing = N
+		g.position.Direction = N
 	}
 }
 
-func (g *Game) peek() rune {
-	next := g.next()
+func (g *Game) peek(next Point) rune {
+	char := g.puzzle[next.y][next.x]
+	// fmt.Printf("next: %v, char: %s\n", next, string(char))
 
-	val := g.puzzle[next.y][next.x]
-	// fmt.Printf("next: %v, val: %s\n", next, string(val))
-
-	return val
+	return char
 }
 
 func (g *Game) move() {
-	g.position = g.next()
+	g.position.Point = g.next()
 	g.traveled[g.position.Token()] = true
 }
 
 func (g *Game) isEnd() bool {
-	if g.position.x == 0 || g.position.y == 0 {
+	if g.position.Point.x == 0 || g.position.Point.y == 0 {
 		return true
 	}
-	if g.position.x == g.xMax || g.position.y == g.yMax {
+	if g.position.Point.x == g.xMax || g.position.Point.y == g.yMax {
 		return true
 	}
 
@@ -122,5 +177,33 @@ func (g *Game) isEnd() bool {
 
 func part1(puzzle [][]rune, x, y int) int {
 	game := New(puzzle, x, y)
-	return game.Simulate()
+	count, _ := game.Simulate()
+	return count
+}
+
+func part2(puzzle [][]rune, xStart, yStart int) int {
+	count := 0
+	for y := 0; y < len(puzzle); y++ {
+		for x := 0; x < len(puzzle[0]); x++ {
+			// skip if already a rock
+			if puzzle[y][x] == '#' {
+				continue
+			}
+			puzzle[y][x] = '#'
+			game := New(puzzle, xStart, yStart)
+			if _, ok := game.Simulate(); !ok {
+				// printPuzzle(p)
+				count++
+			}
+			puzzle[y][x] = '.' // revert
+		}
+	}
+
+	return count
+}
+
+func printPuzzle(puzzle [][]rune) {
+	for _, row := range puzzle {
+		fmt.Println(string(row))
+	}
 }
