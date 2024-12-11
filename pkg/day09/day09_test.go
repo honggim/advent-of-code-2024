84ef447cb79438b9dc7e95837e68f524eff3efee
hong <hong.gim@iheartjane.com>
2024-12-10 21:55:52 -0800

    day 9, part 2: not working

--- pkg/day09/day09_test.go
+++ pkg/day09/day09_test.go
@@ -9,20 +9,45 @@ func TestDay09(t *testing.T) {
 	tests := []struct {
 		name     string
 		filepath string
-		fn       func([]string) int
+		fn       func(string) int
 		expected int
 	}{
+		// {
+		// 	name:     "part 1: example",
+		// 	filepath: "./example.txt",
+		// 	fn:       part1,
+		// 	expected: 1928,
+		// },
+		// {
+		// 	name:     "part 1: what if more than single char",
+		// 	filepath: "./example_30char.txt",
+		// 	fn:       part1,
+		// 	expected: 4006,
+		// },
+		// {
+		// 	name:     "part 1: input",
+		// 	filepath: "./input.txt",
+		// 	fn:       part1,
+		// 	expected: 6386640365805,
+		// },
+		// {
+		// 	name:     "part 2: example",
+		// 	filepath: "./example.txt",
+		// 	// filepath: "./example_plus2.txt",
+		// 	fn:       part2,
+		// 	expected: 2858,
+		// },
 		{
-			name:     "part 1: example",
-			filepath: "./example.txt",
-			fn:       part1,
-			expected: 1928,
+			name:     "part 2: example",
+			filepath: "./example_plus2.txt",
+			fn:       part2,
+			expected: 3261,
 		},
 		{
-			name:     "part 1: input",
+			name:     "part 2: input",
 			filepath: "./input.txt",
-			fn:       part1,
-			expected: 6386640365805,
+			fn:       part2,
+			expected: 2858, // 111371536186 is too low
 		},
 	}
 
@@ -33,7 +58,7 @@ func TestDay09(t *testing.T) {
 				t.Errorf("unexpected err: %v", err)
 			}
 
-			actual := tt.fn(lines)
+			actual := tt.fn(lines[0]) // single line file
 			if actual != tt.expected {
 				t.Errorf("expected: %d, actual: %d", tt.expected, actual)
 			}
